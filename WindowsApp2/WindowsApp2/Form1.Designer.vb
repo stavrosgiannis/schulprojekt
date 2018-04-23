b7d27d9a95dc97f804d7ad35c69f987546cb99ab
Stav ros <hackerone1@web.de>
2018-04-23 17:52:28 +0200

    lol

--- WindowsApp2/WindowsApp2/Form1.Designer.vb
+++ WindowsApp2/WindowsApp2/Form1.Designer.vb
@@ -37,7 +37,7 @@ Partial Class Form1
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(399, 158)
+        Me.Button2.Location = New System.Drawing.Point(597, 70)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(75, 23)
         Me.Button2.TabIndex = 1
