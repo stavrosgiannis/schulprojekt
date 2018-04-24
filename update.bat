26725981a49e73677ba621da95870504e5193a98
stavrosgiannis <stavros.g@web.de>
2018-04-24 19:29:53 +0200

    Update update.bat

--- update.bat
+++ update.bat
@@ -1,8 +1,8 @@
 @echo off
 taskkill /f /im WindowsApp2.exe
-TIMEOUT 2
+TIMEOUT 1
 del "WindowsApp2.exe"
-TIMEOUT 2
+TIMEOUT 1
 ren "update.exe" "WindowsApp2.exe"
-TIMEOUT 2
+TIMEOUT 1
 start /d "%cd%" WindowsApp2.exe
